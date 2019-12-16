2780b6ee2253597f06b1ccc6ea6c3bdfafc0dacf
Andrew Smith <andrewsmith2374@gmail.com>
2019-12-15 21:31:03 -0800

    Update makeGrid.java
    Idk

--- Objects/makeGrid.java
+++ Objects/makeGrid.java
@@ -1,30 +1,11 @@
 import java.util.Random;
+
 //Generates a mineField, 20 by 20 out of object GridSquare, returns as GridSquare[][]
-public static class Grid(){
-  public static GridSquare[][] makeGrid(GridSquare[][] mineField, int numBombs) {
-  	//randomly setting numBombs squares to be bombs
-  	for(int i = 0; i < numBombs; i++){
-  		boolean test = true;
-  		int x = (int)Math.random();
-  		int y = (int)Math.random();
-  		while(test){
-  			//Making sure that the random number is in range of mineField
-  			if((x < mineField.length && x > 0) && (y < mineField[0].length && y > 0)){
-  				//Testing to see whether this square is already a bomb
-  				if(mineField[x][y].getBombStatus() == false){
-  					mineField[x][y].changeBombStatus(true);
-  					test = false;
-  				}
-  			}
-  		  x = (int)Math.random();
-  			y = (int)Math.random();
-  		}
-  	}
-	
-	for(int row = 0; i < mineField.length; row++) {
-		for(int col = 0; col < mineField[0].length; col++) {
-			mineField[row][col].setCoordinates(row, col);
-			mineField[row][col].assignNumber(mineField);
+public class MakeGrid {
+	public static GridSquare[][] makeGrid(GridSquare[][] mineField, int numBombs) {
+		//Declare variables
+		GridSquare tile /*Individual mine*/;
+		
 		//Go through grid, creating new tiles
 		for(int row = 0; row < mineField.length; row++) {
 			for(int col = 0; col < mineField[row].length; col++) {
